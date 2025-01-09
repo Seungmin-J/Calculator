bf1cf6ec70879d45eb799056ac37ab20046aac20
seungminJ <seungmin103@gmail.com>
2025-01-09 13:49:57 +0900

    feat: add feature to return values greater than the input in records

--- src/main/java/com/example/calculator/lv3/App.java
+++ src/main/java/com/example/calculator/lv3/App.java
@@ -1,29 +1,33 @@
 package com.example.calculator.lv3;
 
+import java.util.List;
 import java.util.Scanner;
+
+import static java.lang.Double.parseDouble;
 
 public class App {
     public static void main(String[] args) {
-        ArithmeticCalculator<Number> cal = new ArithmeticCalculator<>();
+        ArithmeticCalculator calculator = new ArithmeticCalculator();
         Scanner sc = new Scanner(System.in);
 
+        // 계산기 앱 실행
         while (true) {
             System.out.println("계산기 실행 !\n");
             // 작업 선택
             System.out.println("1. 계산하기");
             System.out.println("2. 계산기록 확인");
-            int mode = Integer.parseInt(sc.nextLine());
+            System.out.println("3. 기록 삭제하기(가장 먼저 계산된 기록부터)");
             // 계산하기
-            switch (mode) {
+            switch (Integer.parseInt(sc.nextLine())) {
                 case 1:
                     //첫 번째 값 입력
                     System.out.println("첫 번째 값을 입력하세요");
-                    double firstNumber = Double.parseDouble(sc.nextLine());
+                    double firstNumber = parseDouble(sc.nextLine());
                     // 양수만 입력
                     while (firstNumber <= 0) {
                         System.out.println("양수를 입력해주세요\n");
                     }
-                    // 입력값이 연산자가 아니면 다시 입력 반복
+                    // 입력값이 연산자가 아니면 예외처리 후 다시 입력 반복
                     OperatorType operator = null;
                     while (operator == null) {
                         try {
@@ -41,10 +45,10 @@ public class App {
                     while (true) {
                         Double result;
                         System.out.println("두 번째 값을 입력하세요");
-                        lastNumber = Double.parseDouble(sc.nextLine());
-                        // calculate() 메서드 실행
+                        lastNumber = parseDouble(sc.nextLine());
+                        // calculate() 메서드 실행  /  0으로 나눌 때 예외 처리
                         try {
-                            result = cal.calculate(firstNumber, lastNumber, operator);
+                            result = calculator.calculate(firstNumber, lastNumber, operator);
                             System.out.println("결과 : " + result);
                             System.out.println("첫 번째 값 : " + firstNumber + " / " + "두 번째 값 : " + lastNumber + "\n");
                             break;
@@ -58,40 +62,59 @@ public class App {
                         return;
                     }
                     break;
+                // 기록 확인/삭제
                 case 2:
-                    // 기록 확인하기
-                    System.out.println("1. 기록 확인하기");
-                    System.out.println("2. 기록 삭제하기(가장 먼저 계산된 기록부터)");
-
-                    mode = Integer.parseInt(sc.nextLine());
                     // 기록 확인
-                    switch (mode) {
+                    System.out.println("1. 전체 기록 확인하기");
+                    System.out.println("2. 입력한 값보다 큰 값의 기록 확인하기");
+                    switch (Integer.parseInt(sc.nextLine())) {
+                        // 전체 기록 출력
                         case 1:
-                            if (cal.getRecords().isEmpty()) {
+                            // 기록 없으면 break
+                            if (calculator.getRecords().isEmpty()) {
                                 System.out.println("계산 기록이 없습니다");
                                 break;
                             }
                             System.out.println("계산 기록 순서 :");
-                            for (int i = 0; i < cal.getRecords().size(); i++) {
-                                System.out.println(i + 1 + ". " + cal.getRecords().get(i));
+                            for (int i = 0; i < calculator.getRecords().size(); i++) {
+                                System.out.println(i + 1 + ". " + calculator.getRecords().get(i));
                             }
                             break;
                         case 2:
-                            if (cal.getRecords().isEmpty()) {
+                            // 입력 값보다 큰 값의 기록 출력
+                            // 기록 없으면 break
+                            if (calculator.getRecords().isEmpty()) {
                                 System.out.println("계산 기록이 없습니다");
                                 break;
-                            } else {
-                                cal.getRecords().remove(cal.getRecords().get(0));
-                                System.out.println("삭제되었습니다");
-                                break;
                             }
+                            System.out.println("값을 입력하세요");
+                            double input = Double.parseDouble(sc.nextLine());
+                            List<Number> records = calculator.getGreaterRecordsThanInput(input);
+                            if(!records.isEmpty()) {
+                                System.out.println("계산 기록 순서 :");
+                                for (int i = 0; i < records.size(); i++) {
+                                    System.out.println(i + 1 + ". " + records.get(i));
+                                }
+                            }else {
+                                System.out.println("입력값보다 큰 값이 없습니다");
+                            }
+                            break;
                         default:
-                            System.out.println("1번과 2번 중에 선택하세요");
+                            System.out.println("보기의 숫자중에서 하나를 입력하세요");
                             break;
                     }
                     break;
+                case 3:
+                    // 기록 없으면 break
+                    if (calculator.getRecords().isEmpty()) {
+                        System.out.println("계산 기록이 없습니다");
+                        break;
+                    }
+                    calculator.getRecords().remove(calculator.getRecords().get(0));
+                    System.out.println("삭제되었습니다");
+                    break;
                 default:
-                    System.out.println("1번과 2번 중에 선택하세요");
+                    System.out.println("보기의 숫자중에서 하나를 입력하세요");
                     break;
             }
         }
